6685bfa9698dea6a09e1af726884c7b05c4c6f9b
Alessio Demo <alessio.demo@studenti.unipd.it>
2025-10-25 21:29:21 +0200

    fixes for correct evaluation of WORLD dataset

--- src/db/run_queries_to_json.py
+++ src/db/run_queries_to_json.py
@@ -40,7 +40,15 @@ def execute_queries_and_save_json(con, queries, output_dir):
             columns = [desc[0] for desc in result.description]
 
             # Convert result to list of dictionaries
-            data = [dict(zip(columns, row)) for row in rows]
+            data = []
+            for row in rows:
+                row_dict = dict(zip(columns, row))
+
+                for key, value in row_dict.items():
+                    if ("sum(" in key or "avg(" in key) and isinstance(value, int):
+                        row_dict[key] = float(value)
+
+                data.append(row_dict)
 
             # Create JSON filename based on SQL file name
             json_name = f"query{i}.json"
@@ -61,7 +69,23 @@ def run_queries_to_json(dataset_name: str) -> None:
     qrs = load_queries_from_folder(data_dir)
 
     if qrs:
-        execute_queries_and_save_json(con, qrs, SUBMISSIONS_PATH)
+        complete_path = SUBMISSIONS_PATH / f"{dataset_name}"
+        execute_queries_and_save_json(con, qrs, complete_path)
     else:
         print(f"No queries found for dataset '{dataset_name}'")
 
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("❌ Specifica il nome del dataset come argomento.")
+        print("Esempio: python3 main.py test_dataset")
+        return
+
+    dataset_name = sys.argv[1]
+    print(f"▶️ Avvio test per il dataset: {dataset_name}")
+    run_queries_to_json(dataset_name)
+    print(f"✅ Test completato per il dataset: {dataset_name}")
+
+if __name__ == "__main__":
+    main()
